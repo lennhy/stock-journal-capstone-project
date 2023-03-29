290a6bc0a2eddeb433f721bdfe3a376be6db0ae8
Lenn <lhypolite@brainstation.io>
2023-03-29 18:35:11 -0400

    Add line of code for serving static files and comment it out.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+// app.use(express.static("public")); // set up our Express server to serve static files
+
 const stockTotalsRoute = require("./routes/stockTotalsRoute");
 
 app.use((req, res, next) => {
