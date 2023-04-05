db5e97eab5b128767aedf4af8d76b99706e2456d
Lenn <lhypolite@brainstation.io>
2023-04-05 18:14:14 -0400

    Add use effect and an axios and get request to get brokerage trade data from node api.

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
+useEffect(() => {
+  axios.get("http://localhost:8080/").then((response) => {
+    // use .map to group the onbjects in the array by date in ascending order
+  });
+}, []);
+
 const Calendar = () => {
+  useEffect;
   return (
     <div>
       <p>Hello</p>
